c716e149e7ee4b5a93d7dc8a5ae1533fd9853c12
Mark Paris <mparis@lanl.gov>
2019-06-05 21:14:18 -0600

    Repairing setup.py syntax error.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = [
-        'description' : 'My Project',
-        'author' : 'My Name',
-        'url' : 'https://github.com/mwparis/pythonProjectTemplate.git',
-        'download_url' : 'https://github.com/mwparis/pythonProjectTemplate.git',
-        'author_email' : 'mparis@lanl.gov',
-        'version' : '0.1',
-        'install_requires' : ['nose'],
-        'packages' : ['NAME'],
-        'scripts' : [],
-        'name' : 'generic_project_template'
+        'description': 'My Project',
+        'author': 'Mark Paris',
+        'url': 'https://github.com/mwparis/pythonProjectTemplate.git',
+        'download_url': 'https://github.com/mwparis/pythonProjectTemplate.git',
+        'author_email': 'mparis@lanl.gov',
+        'version': '0.1',
+        'install_requires': ['nose'],
+        'packages': ['NAME'],
+        'scripts': [],
+        'name': 'generic_project_template'
 ]
 
 setup(**config)
